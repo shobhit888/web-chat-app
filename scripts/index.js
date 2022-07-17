45f666da4141dd18f9fc821bb6c49adba809864b
shobhit888 <101204659+shobhit888@users.noreply.github.com>
2022-07-17 14:35:14 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 const numberOfChats = 10;
 let fakeChats = [];
-// generate an array of fake chats to show in app
+
 for (let i = 1; i < numberOfChats; i++) {
     fakeChats.push(chatGenerator(i))
 }
 
-// this is the signed-in user object
+
 const authedUser = {
     id: '12',
     name: "Behnam Azimi",
@@ -15,17 +15,15 @@ const authedUser = {
     avatar: "https://randomuser.me/api/portraits/men/1.jpg"
 };
 
-// create instance of ChatApp,
-// this is the line that run application
+
 const app = new ChatApp("chat-web-app");
 app.signin(authedUser);
 
-// add all generated chats to app one-by-one
+
 fakeChats.map(fc => app.addChat(fc));
 
 
-// below code is just for simulating message receive
-// here we send 100 messages in different times ro app
+
 let fakeMsgCounter = 100;
 const interval = setInterval(() => {
 
@@ -39,7 +37,7 @@ const interval = setInterval(() => {
         if (!fakeSender)
             return;
 
-        // flag with a 20% probability
+        
         const randomFlag = Math.random() > .8;
 
         app.newMessage({
@@ -49,7 +47,7 @@ const interval = setInterval(() => {
             toChat: randomFlag ? fakeSender.id : authedUser.id
         });
 
-        // new message sending time can be dynamic, between 1s and 5s
+        
     }, randomNumber(1000, 5000))
 
 }, 1500);
